ac60e2b7ded22851f84a242813a36a937340d8bf
marcosdoerner <marcosdoerner@gmail.com>
2020-06-04 00:25:30 -0300

    Jogo cobrinha, exemplo.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aula_javascript
 Introdução ao JavaScript para treinamento da Digital Innovation One
+
+Inclusão no exemplo do jogo da cobrinha em Javascript
---
+++ js/main.js
@@ -0,0 +1,87 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML ="<b>Obrigado por clicar.</b>";
+    //alert("Obrigado por clicar.");
+}
+
+function redirecionar(){
+    window.open("https://globallabs.academy/");
+}
+
+
+// function validaIdade(idade){
+//     return (idade >= 18);
+// }
+
+// //var idade = prompt("Qual sua idade?");
+// alert(validaIdade(idade));
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+
+// function setReplace(frase, nome, novo_nome){
+//     return frase.replace(nome, novo_nome);
+// }
+
+//alert(setReplace("japão lindo", "japão", "Brasil"));
+//alert(soma(5,10));
+/*
+
+var lista  = [nome: “rosa” , “cravo” , “tulipa”, “margarida”]
+
+console.log(lista[2]);
+
+*/
+// trajeto = ["mãe", "floresta", "avó"];
+// console.log(trajeto.toString());
+
+// var d = new Date();
+
+// alert(d.getUTCMonth());
+
+
+// var count;
+
+// for(count=0; count <= 5; count++){
+//     console.log(count);
+// }
+
+
+// while (count < 5){
+//     console.log(count);
+//     count++;
+// }
+
+
+// var idade = prompt("Qual sua idade?");
+// if (idade >= 18){
+//     alert("maior de idade");
+// }else {
+//     alert("menor de idade");
+// }
+// var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+// console.log(frutas);
+
+
+// var fruta = {nome:"maça", cor:"vermelha"};
+// console.log(fruta);
+
+
+// var lista = ["maça", "pera", "laranja"];
+// console.log(lista);
+// lista.push("uva");
+// console.log(lista.join(" | "));
+
+// lista.pop();
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString())
+
+// var nome = "Marcos Doerner";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é uma ilha";
+//alert(nome);
+
+
